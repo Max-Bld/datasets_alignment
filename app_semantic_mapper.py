cebcc59a4ef03a8c2eab3e50e26466ad466d5cb0
max-bld <mblanchard95@gmail.com>
2024-04-30 17:26:58 +0200

    semantic mapper dev

--- app_semantic_mapper.py
+++ app_semantic_mapper.py
@@ -7,6 +7,16 @@ Created on Tue Apr 30 16:49:42 2024
 """
 
 import streamlit as st
+from rdflib import Graph
+from ontology_flatter import flattened_ontology
+from schema_extractor import columns
 
 st.title('Eona-X Semantic Mapper')
 
+flattened_ontology.keys()
+
+option = st.selectbox(
+    'Columns of CSV dataset',
+    (columns))
+
+st.write('You selected:', option)
